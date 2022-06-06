f0bee67ec3235aa563c7e1236e064c340d05cf26
MattPingID <matthewmanisco@pingidentity.com>
2022-06-06 16:58:56 -0700

    exception handling for connection errors

--- pd-bulk-api-tool.py
+++ pd-bulk-api-tool.py
@@ -13,10 +13,14 @@ from getpass import getpass
 
 requests.packages.urllib3.disable_warnings() #Disblae invalid cert warnings
 
-options = { 1: 'Add LDAP Entries',
-            2: 'Delete LDAP Entries',
-            3: 'Modify LDAP Entries',
-            4: 'Exit'  }
+options = { 1: "Add LDAP Entries",
+            2: "Delete LDAP Entries",
+            3: "Modify LDAP Entries",
+            4: "Exit"  }
+
+ldap_modification_options = { 1: "Update an Entry (HTTP PUT) - Operates exclusively on top level attributes.  Use for renaming entires, changing Parent DNs, CN, UID, SN, naming attribiutes ",
+                              2: "Update an Entry (HTTP PATCH) - Functions like an ldapmodify operation", 
+                              3: "Return to main menu" }
 
 def main():
     try:
@@ -37,7 +41,7 @@ def main():
                 pass
 
             if (menu_selection == 1):
-                print("\n* Select input file containing entries to add.\n* Input file must be either json or raw text consisting of one entery per line enclosed in braces\n\nExample:\n{\"_dn\": \"uid=user.0,ou=people,dc=example,dc=com\",\"objectClass\": [\"top\", \"organizationalPerson\", \"inetOrgPerson\", \"pf-connected-identities\"],\"sn\": [\"Seawell\"],\"cn\": [\"Esko Seawell\"],\"givenName\": [\"Esko\"],\"uid\": [\"user.0\"],\"mail\": [\"user.0@example.com\"],\"userPassword\": [\"2FederateM0re\"],\"pf-connected-identity\": [\"auth-source=pf-local-identity:user-id=user.0\"]}\n")  
+                print("\n* Select input file containing entries to add.\n* Input file must be either json or raw text consisting of one entery per line enclosed in braces\n\nExample:\n\n{\"_dn\": \"uid=user.0,ou=people,dc=example,dc=com\",\"objectClass\": [\"top\", \"organizationalPerson\", \"inetOrgPerson\", \"pf-connected-identities\"],\"sn\": [\"Seawell\"],\"cn\": [\"Esko Seawell\"],\"givenName\": [\"Esko\"],\"uid\": [\"user.0\"],\"mail\": [\"user.0@example.com\"],\"userPassword\": [\"2FederateM0re\"],\"pf-connected-identity\": [\"auth-source=pf-local-identity:user-id=user.0\"]}\n")  
                 input_file = select_input_file()
                 add_ldap_entries(input_file, pd_api_base_url, headers, login_credentials)
     
@@ -47,11 +51,18 @@ def main():
                 delete_ldap_entries(input_file, pd_api_base_url, headers, login_credentials)
     
             elif(menu_selection == 3):
-                print("\n* Select input file containing desired modifactions for existing LDAP entires.\n* Input file must be valid json with a single \"entriesToModify\" key with a value consisting of a list of DN objects to modify and corresponding \"modifcations\"  key for each entry\n")
-                input_file = select_input_file()
-                print_modification_type_menu()
-                modify_ldap_entries(input_file, pd_api_base_url, headers, login_credentials)
-    
+                print_mdoification_method_menu()
+                try:
+                    method = int(input("\nSelect Option: "))
+                except (UnboundLocalError, Exception):
+                    pass
+                if (method == 3):
+                    pass
+                else:
+                    print("\n* Select input file containing desired modifactions for existing LDAP entires.\n* Input file must be valid json with a single \"entriesToModify\" key with a value consisting of a list of DN objects to modify and corresponding \"modifcations\"  key for each entry\n")
+                    input_file = select_input_file()
+                    modify_ldap_entries(input_file, pd_api_base_url, headers, login_credentials, method)
+
             elif (menu_selection == 4):
                 print("\nExiting")
                 exit()
@@ -64,7 +75,6 @@ def main():
     except KeyboardInterrupt:
         sys.exit(0)
 
-
 ##
 
 
@@ -74,13 +84,11 @@ def print_menu():
         print("\t\t",key,"--",options[key]) 
 
 
-
 def select_input_file():
     root = tk.Tk()
     root.withdraw()
     file_path = filedialog.askopenfilename()
     return file_path
-
 
 
 def validate_json(input_file):        
@@ -92,25 +100,21 @@ def validate_json(input_file):
     return True
 
 
-
 def get_ldap_creds():
     while(True):
         try: 
             default_admin_acct = input("Log in with 'cn=Administrator'? (y/n): ")
-    
             if (default_admin_acct == 'y') or (default_admin_acct == 'Y'):
                 login = "cn=Administrator"
             elif (default_admin_acct == 'n') or (default_admin_acct == 'N'):
                 login = input("Login: ")    
             else:
                  raise ValueError
-            
             password = "2FederateM0re" #getpass("Admin Password: ")
             return (login, password)
        
         except ValueError:
             print("Invalid Entry.  Enter y/n!\n")
-
 
 
 def add_ldap_entries(input_file, pd_api_base_url, headers, login_credentials):
@@ -139,14 +143,14 @@ def add_ldap_entries(input_file, pd_api_base_url, headers, login_credentials):
                 else:
                     print(api_response.json())
                 time.sleep(0.05)    
+    
+    except requests.exceptions.ConnectionError:
+            print("\nConnection Error: Unable to connect to PingDirectory API at {}".format(pd_api_base_url))
     except IOError:
         print("Invalid File")
 
 
-
 def delete_ldap_entries(input_file, pd_api_base_url, headers, login_credentials):
-    while(True):
-    
         try:
             if not input_file:
                 return
@@ -172,14 +176,19 @@ def delete_ldap_entries(input_file, pd_api_base_url, headers, login_credentials)
                     else:
                         print(api_response.json())
                     time.sleep(0.05)
-                break
+        except requests.exceptions.ConnectionError:
+            print("\nConnection Error: Unable to connect to PingDirectory API at {}".format(pd_api_base_url))                     
         except IOError:
             print("\n\nInvalid File!\n")
-            input_file = select_input_file()
+        
 
 
+def print_mdoification_method_menu():
+    print("\n\n\t\t\t* * * * Menu * * * *\n")
+    for key in ldap_modification_options.keys():
+        print("\t\t",key,"--",ldap_modification_options[key]) 
 
-def modify_ldap_entries(input_file, pd_api_base_url, headers, login_credentials):
+def modify_ldap_entries(input_file, pd_api_base_url, headers, login_credentials, method):
     while(True):
         try:
             if not input_file:
@@ -212,6 +221,8 @@ def modify_ldap_entries(input_file, pd_api_base_url, headers, login_credentials)
         except (IOError):
             print("\n\nInvalid File!\n")
             input_file = select_input_file()
+
+
             
 ##
 
